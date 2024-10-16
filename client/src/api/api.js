acd56f8a33bd4a37ec93f0ad3f1d4686548d53f8
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-10-16 12:09:20 +0530

    changes

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
- // const local = 'http://localhost:5000'
-const production = 'https://multi-vendor-site.onrender.com'
+  const local = 'http://localhost:5000'
+//const production = 'https://multi-vendor-site.onrender.com'
 const api = axios.create({
-    baseURL: `${production}/api`,
+    baseURL: `${local}/api`,
     withCredentials : true
 })
 export default api
